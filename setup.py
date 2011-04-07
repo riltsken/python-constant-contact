7ad6a8e3ef504a7b357a34b5b578f1f4c9daa75d
Stephen <subsume@gmail.com>
2011-04-07 19:14:11 +0000

    roerganized project

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+f = open('README')
+readme = f.read()
+f.close()
+
+setup(
+    name='python-constantcontact',
+    version='0.5.3',
+    description='Constant contact lib for python, with stuff for Django too',
+    author='Sam Toriel',
+    author_email='storiel@gmail.com',
+    url='http://github.com/riltsken/python-constantcontact/tree/master',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
