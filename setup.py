1e1b9b0cc8db9fe1113c8964d607120a82cee790
Samuel Toriel <samueltoriel@gmail.com>
2011-05-23 11:04:00 -0700

    corrected the author email

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='0.1',
     description='Constant contact lib for python, with stuff for Django too',
     author='Sam Toriel',
-    author_email='storiel@gmail.com',
+    author_email='samueltoriel@gmail.com',
     url='http://github.com/riltsken/python-constantcontact/tree/master',
     packages=find_packages(),
 	install_requires=['httplib2','feedparser'],
